a301b17071f9e1e47abb6b7a9c89178156e0a1e1
damat-le@main-cnr <leo.damato.dev@gmail.com>
2025-07-13 01:59:55 +0200

    add unet_seq model (0.41)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,13 @@
 from .ae.ae import Autoencoder
 from .unet.unet import UNet
+from .unet.unet_seq import UNetSequential
 from .resnet.resnet import ResNet, ResNetPatch
 from .random.random import RandomModel
 
 MODEL_REGISTRY = {
     "autoencoder": Autoencoder,
     "unet": UNet,
+    "unet_seq": UNetSequential,
     "resnet": ResNet,
     "resnet_patch": ResNetPatch,
     "random": RandomModel
