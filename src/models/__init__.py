05bc4d6b378185fa11591bcbaa8ced5d6a8c1687
damat-le@conan-gpu <leo.damato.dev@gmail.com>
2025-08-09 12:05:57 +0200

    delete useless files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,16 +1,7 @@
-from .ae.ae import Autoencoder
-from .unet.unet import UNet
 from .unet.unet_seq import UNetSequential
-from .resnet.resnet import ResNet, ResNetPatch
-from .random.random import RandomModel
 from .positional.posnet import PosNet
 
 MODEL_REGISTRY = {
-    "autoencoder": Autoencoder,
-    "unet": UNet,
     "unet_seq": UNetSequential,
-    "resnet": ResNet,
-    "resnet_patch": ResNetPatch,
-    "random": RandomModel,
     "posnet": PosNet
 }
