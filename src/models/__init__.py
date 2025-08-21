09930d5324ca442bc3fc81c7872415b10ce82855
damat-le@conan-gpu <leo.damato.dev@gmail.com>
2025-08-21 15:02:37 +0200

    this adds two more models that do not work at the moment. They are based on the ideas from the paper https://arxiv.org/abs/2201.03545

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,14 @@
 from .unet.unet_seq import UNetSequential
 from .positional.posnet import PosNet
 from .pipeline.e2e import Pipeline
+from .convnext.convnext import ConvNextFull, ConvNextSeq
+from .unext.unext import UNext
 
 MODEL_REGISTRY = {
     "unet_seq": UNetSequential,
     "posnet": PosNet,
-    "pipeline": Pipeline
+    "pipeline": Pipeline,
+    "convnext": ConvNextFull,
+    "convnext_seq": ConvNextSeq,
+    "unext": UNext
 }
---
+++ src/models/convnext/train.py
@@ -0,0 +1,208 @@
+if __name__ == "__main__":
+
+    import time
+    import torch
+    import numpy as np
+    from src.torch_dataset import load_flowers_dataset, SwirledDataset
+    from torch.utils.data import DataLoader
+    from torch.optim import AdamW
+    from torch.optim.lr_scheduler import CosineAnnealingLR
+    from tqdm import tqdm
+
+    from argparse import ArgumentParser
+    from src.utils.config import Config
+    from src.utils.logger import MyLogger
+    from src.models import MODEL_REGISTRY
+    from src.eval import eval_swirl_mask, generate_samples
+    
+
+    # -------------------------------------------
+    # Load config
+    # -------------------------------------------
+    parser = ArgumentParser()
+    parser.add_argument("-c", type=str, required=True)
+    args = parser.parse_args()
+    c = Config.from_yaml(args.c)
+
+
+    # -------------------------------------------
+    # Setup logger
+    # -------------------------------------------
+    logger = MyLogger(c.log_params["log_dir"])
+    logger.log_config(c)
+
+
+    # -------------------------------------------
+    # Setup device
+    # -------------------------------------------
+    device = torch.device(c.trainer_params["device"])
+
+
+    # -------------------------------------------
+    # Setup datasets
+    # -------------------------------------------
+    data, labels = load_flowers_dataset(
+        c.data_params["in_dir"]
+    )
+
+    val_idx = np.random.choice(
+        len(data),
+        c.val_params["val_size"]
+    )
+
+    val_data =[]
+    val_labels = []
+    for i in val_idx:
+        val_data.append(data[i])
+        val_labels.append(labels[i])
+    
+    data = [data[i] for i in range(len(data)) if i not in val_idx]
+    labels = [labels[i] for i in range(len(labels)) if i not in val_idx]
+    
+    ds = SwirledDataset(data)
+
+    dl = DataLoader(
+        ds, 
+        shuffle=True,
+        batch_size=c.trainer_params["batch_size"], 
+        num_workers=c.trainer_params["data_workers"],
+    )
+
+    val_ds = SwirledDataset(val_data)
+
+    val_dl = DataLoader(
+        val_ds, 
+        shuffle=False,
+        batch_size=c.val_params["batch_size"], 
+        num_workers=c.val_params["data_workers"],
+    )
+
+    # -------------------------------------------
+    # Initialize the model
+    # -------------------------------------------
+    architecture = MODEL_REGISTRY[c.model_params.pop("model_name")]
+    model = architecture(**c.model_params)
+    model = model.to(device)
+
+    
+    # -------------------------------------------
+    # Initialize the optimizer
+    # -------------------------------------------
+
+    optimizer = AdamW(
+        model.parameters(),
+        lr=c.opt_params["lr"],
+        weight_decay=c.opt_params["weight_decay"]
+    )
+
+    # -------------------------------------------
+    # Initialize the LR scheduler
+    # -------------------------------------------
+    
+    scheduler = CosineAnnealingLR(
+        optimizer,
+        T_max=c.lr_scheduler_params["T_max"]
+    )
+
+    # -------------------------------------------
+    # Print the model summary
+    # -------------------------------------------
+    print('----------------------------------------')
+    print(f'Model: {architecture.__name__.split(".")[-1]}')
+    print(f"Num. of params: {sum(p.numel() for p in model.parameters())}")
+    print(f'Device: {c.trainer_params["device"]}')
+    print('----------------------------------------')
+
+  
+    # -------------------------------------------
+    # Training cycle
+    # -------------------------------------------
+
+    pbar = tqdm(total=len(dl), desc="Training")
+    it = 0
+    for epoch in range(c.trainer_params["epochs"]):
+        model.train()
+
+        if epoch < 5:
+            top_k = 64
+        elif epoch < 100:
+            top_k = 16
+        elif epoch < 150:
+            top_k = 8
+        else:
+            top_k = 4
+
+        END_TIME = time.time()
+        for batch in dl:
+
+            # monitor the time to load a batch
+            data_time = time.time() - END_TIME
+
+            swirled, mask, original = batch
+            swirled = swirled.to(device)
+            mask = mask.to(device)
+            original = original.to(device)
+
+            optimizer.zero_grad()
+            output = model(swirled)
+            loss, other_info = model.loss_function_patch(output, original, top_k=top_k)
+            
+            logger.log_scalars(
+                scalars={
+                    'LR': optimizer.param_groups[0]['lr'],
+                    'Epoch': epoch,
+                    **other_info
+                },
+                step=it
+            )
+
+            if it % 1 == 0:
+                pbar.set_postfix({
+                    "Epoch": epoch,
+                    "Loss": loss.item(),
+                    "DataTime": f"{data_time:.2f}s",
+                })
+
+            loss.backward()
+            optimizer.step()
+
+            pbar.update(1)
+            it += 1
+
+            END_TIME = time.time()
+        # -------------------------------------------
+        # Validation
+        # -------------------------------------------
+
+        model.eval()
+
+        val_results = eval_swirl_mask(
+            dataloader=val_dl, 
+            model=model
+        )
+
+        logger.log_scalars(
+            scalars=val_results,
+            step=it
+        )
+
+        generate_samples(
+            log_dir=logger.samples_dir,
+            dataset=val_ds,
+            model=model,
+            num_samples=5
+        )
+        # -------------------------------------------
+        # Save checkpoint
+        # -------------------------------------------
+        logger.log_checkpoint(
+            model=model,
+            step=it,
+            optimizer=None,
+            scheduler=None
+        )
+
+        scheduler.step()
+
+        pbar.reset()
+    pbar.close()
